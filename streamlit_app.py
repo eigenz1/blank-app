408453151b948b26fb5d3e8bbc46e7c640e8aef6
eigenz1 <60969850+eigenz1@users.noreply.github.com>
2025-01-09 12:43:38 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 이상한 나라의 초대장")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
